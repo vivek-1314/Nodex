bd40109b8ea3170f95c895f95df08fe3087b2503
DRE_1314 <vivek1314gurjar@gmail.com>
2025-03-26 22:29:52 +0530

    remove secrets

--- backend/firebaseadmin.js
+++ backend/firebaseadmin.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../backend/config/nodex-de76d-firebase-adminsdk-fbsvc-4df782ce0c.json");
+const serviceAccount = require("../backend/config/firebasebackendconfig.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
