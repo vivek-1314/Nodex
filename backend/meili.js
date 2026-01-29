ceeafb3e2109a9a20fccf64e81e10bb49aaad198
DRE_1314 <vivek1314gurjar@gmail.com>
2026-01-29 16:15:06 +0530

    meili search issue slolved

--- backend/meili.js
+++ backend/meili.js
@@ -2,7 +2,7 @@ const { MeiliSearch } = require("meilisearch");
 require("dotenv").config();
 
 const client = new MeiliSearch({
-  host: "https://ms-66d57f8fb39e-23456.lon.meilisearch.io", 
+  host: "https://ms-646e311edd98-39404.fra.meilisearch.io", 
   apiKey: process.env.MEILI_MASTER_KEY, 
 });
 
